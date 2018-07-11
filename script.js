5592b8bd930a03e25f2c7c77468045c32a4cf21b
Olessia Potapova <olessia.potapova@gmail.com>
2018-07-11 12:02:35 -0300

    handle functions for number, math and flush operation

--- script.js
+++ script.js
@@ -9,25 +9,74 @@ document.querySelector(".calc-buttons").addEventListener("click", function(event
     
 function buttonClick(value) {
     if(isNaN(parseInt(event.target.innerText))) {
-        handleOperator(event);
+        handleOperator(value);
     } else {
         handleNumber(value);
     }    
+    rerender();
 } 
+
 function handleOperator(value) {
-    //console.log(value);
+    switch(value) {
+        case 'C':   
+            buffer = "0";
+            runningTotal = 0;
+            break; 
+        case "=":
+            if(previousOperator === null) {
+                return;
+            }
+            flushOperation(parseInt(buffer));
+            buffer = "" + runningTotal;
+            runningTotal = 0;
+            break;
+        case "⬅":
+            if(buffer.length === 1) {
+                buffer = "0"
+            } else {
+                buffer = buffer.substring(0, buffer.length - 1);
+            }
+        default:
+            handleMath(value);
+            break;
+    }
 };
 
 function handleNumber(value) {
     if(buffer === "0") {
         buffer = value;
-        console.log(buffer);
     } else {
         buffer += value;
     }
-    render();
 };
 
-function render() {
+function rerender() {
     screen.innerText = buffer;
+}
+
+function handleMath(value) {
+    const intBuffer = parseInt(buffer);
+
+    if(runningTotal === 0) {
+        runningTotal = intBuffer;
+    } else {
+        flushOperation(intBuffer);
+    }
+   
+    previousOperator = value;
+
+    buffer = "0";
+}
+
+function flushOperation(intBuffer) {
+    console.log("here now");
+    if(previousOperator === '+') {
+        runningTotal += intBuffer;
+    } else if (previousOperator === '-') {
+        runningTotal -= intBuffer;
+    } else if (previousOperator === '/') {
+        runningTotal /= intBuffer;
+    } else if (previousOperator === 'X') {
+        runningTotal *= intBuffer;
+    }
 }
